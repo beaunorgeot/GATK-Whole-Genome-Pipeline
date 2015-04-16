20f54eba0a105a1d230deccb12648c6943be357c
Beau Norgeot <beaunorgeot@gmail.com>
2015-04-15 17:09:47 -0700

    added variant recalibration, changed references

--- GATKpipe.sh
+++ GATKpipe.sh
@@ -4,70 +4,44 @@
 # The pipeline takes unprocessed bam file(s) as input and outputs the biological genomic variance 
 # of the input to a given reference
 
-# See the associated setup.sh and README
+# Run the GATKsetup.sh script first. See the associated README
 
-cd /mnt2
-# default 
-# change to ubuntu linux ami /ephemeral/mnt
-
-# get NA12878 from 1000g from BD2K's s3
-~/s3cmd/s3cmd get \
-    --secret_key=${SECRET_KEY} \
-    --access_key=${ACCESS_KEY} \
-    s3://bd2k-test-data/NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam
-mv NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam NA12878.bam
-
-# get reference genome                                                                                                                                                                                        
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta.fai
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta
-
-# get Known indels
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/1000G_phase1.indels.hg19.sites.fixed.vcf
-
-# get db snp known sites
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/dbsnp_138.hg19.fixed.vcf
-mv dbsnp_138.hg19.fixed.vcf dbsnp_138.vcf
-
+# stderr redirected to descriptively named files.report
 
 cd ~
-#TOOLS
-# get/install samtools
-sudo apt-get install zlib1g-dev
-sudo apt-get install samtools
 
-# get/install picard
-wget https://github.com/broadinstitute/picard/releases/download/1.130/picard-tools-1.130.zip
-unzip picard-tools-1.130.zip
-
-# get GATK.jar
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/GenomeAnalysisTK.jar
+# Set RAM to use for all java processes
+RAM=-Xmx200g
+# Set number of threads to the number of cores/machine for speed optimization
+THREADS=32
 
 # index bam file
-samtools index /mnt2/NA12878.bam
+samtools index /ephemeral/mnt/NA12878.bam
 
 #START PIPELINE
 # calculate flag stats using samtools
 time samtools flagstat \
-    /mnt2/NA12878.bam
+    /ephemeral/mnt/NA12878.bam
 
 # sort reads in picard
-time java -Xmx200g \
+time java $RAM \
     -jar ~/picard/dist/picard.jar \
     SortSam \
-    INPUT=/mnt2/NA12878.bam \
-    OUTPUT=/mnt2/NA12878.sorted.bam \
+    INPUT=/ephemeral/mnt/NA12878.bam \
+    OUTPUT=/ephemeral/mnt/NA12878.sorted.bam \
     SORT_ORDER=coordinate
-rm -r /mnt2/NA12878.bam
+    > sortReads.report 2>&1
+rm -r /ephemeral/mnt/NA12878.bam
 
 # mark duplicates reads in picard
-time java -Xmx200g \
+time java $RAM \
     -jar ~/picard/dist/picard.jar \
     MarkDuplicates \
-    INPUT=/mnt2/NA12878.sorted.bam \
-    OUTPUT=/mnt2/NA12878.mkdup.bam \
+    INPUT=/ephemeral/mnt/NA12878.sorted.bam \
+    OUTPUT=/ephemeral/mnt/NA12878.mkdup.bam \
     METRICS_FILE=/dev/null #File to write duplication metrics to, Required
-rm -r /mnt2/NA12878.sorted.bam
+    > markDups.report 2>&1
+rm -r /ephemeral/mnt/NA12878.sorted.bam
 
 # GATK Indel Realignment
 # There are 2 steps to the realignment process
@@ -75,74 +49,79 @@ rm -r /mnt2/NA12878.sorted.bam
 # 2. Running the realigner over those intervals (IndelRealigner)
 
 # index reference genome, There's no need for this, we have the .fai file
-# samtools faidx /mnt2/human_g1k_v37.fasta
+# samtools faidx /ephemeral/mnt/human_g1k_v37.fasta
 
 # create sequence dictionary for reference genome
-# We could skip this and just download the hg19.dict
-# https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.dict
-java -Xmx200g \
+# We could skip this step. I've download the b37.dict
+
+java $RAM \
     -jar ~/picard/dist/picard.jar \
     CreateSequenceDictionary \
-    REFERENCE=/mnt2/Homo_sapiens_assembly19.fasta \
-    OUTPUT=/mnt2/Homo_sapiens_assembly19.fasta.dict 
+    REFERENCE=/ephemeral/mnt/human_g1k_v37_decoy.fasta \
+    OUTPUT=/ephemeral/mnt/human_g1k_v37_decoy.fasta.dict 
 
 # create target indels (find areas likely in need of realignment)
-time java -Xmx200g \
+time java $RAM \
     -jar ~/gatk-protected/target/GenomeAnalysisTK.jar \
     -T RealignerTargetCreator \
-    -known /mnt2/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf \
-    -known /mnt2/1000G_phase1.indels.hg19.sites.fixed.vcf \
-    -R /mnt2/Homo_sapiens_assembly19.fasta \
-    -I /mnt2/NA12878.mkdups.bam \
-    -o /mnt2/target_intervals.list \
-    -nt 32 #number of threads/cores for task
+    -known /ephemeral/mnt/Mills_and_1000G_gold_standard.indels.b37.vcf \
+    -known /ephemeral/mnt/1000G_phase1.indels.b37.vcf \
+    -R /ephemeral/mnt/human_g1k_v37_decoy.fasta \
+    -I /ephemeral/mnt/NA12878.mkdups.bam \
+    -o /ephemeral/mnt/target_intervals.list \
+    -nt $THREADS 
+    > targetIndels.report 2>&1
 
 # realign reads
-time java -Xmx200g \
+time java $RAM \
     -jar ~/gatk-protected/target/GenomeAnalysisTK.jar \
     -T IndelRealigner \
-    -known /mnt2/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf \
-    -known /mnt2/1000G_phase1.indels.hg19.sites.fixed.vcf \
-    -R /mnt2/Homo_sapiens_assembly19.fasta \
-    -targetIntervals /mnt2/target_intervals.list \
-    -I /mnt2/NA12878.mkdup.bam \
-    -o /mnt2/NA12878.realigned.bam \
-    -nt 32 
-rm -r /mnt2/NA12878.mkdup.bam
+    -known /ephemeral/mnt/Mills_and_1000G_gold_standard.indels.b37.vcf \
+    -known /ephemeral/mnt/1000G_phase1.indels.b37.vcf \
+    -R /ephemeral/mnt/human_g1k_v37_decoy.fasta \
+    -targetIntervals /ephemeral/mnt/target_intervals.list \
+    -I /ephemeral/mnt/NA12878.mkdup.bam \
+    -o /ephemeral/mnt/NA12878.realigned.bam \
+    -nt $THREADS 
+    > realignIndels.report 2>&1
+rm -r /ephemeral/mnt/NA12878.mkdup.bam
 
 # GATK BQSR
 
 # create gatk recalibration table
-time java -Xmx200g \
+time java $RAM \
     -jar ~/gatk-protected/target/GenomeAnalysisTK.jar \
     -T BaseRecalibrator \
-    -R /mnt2/Homo_sapiens_assembly19.fasta \
-    -I /mnt2/NA12878.bam \
-    -knownSites /mnt2/dbsnp_138.vcf \
-    -knownSites /mnt2/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf \
-    -knownSites /mnt2/1000G_phase1.indels.hg19.sites.fixed.vcf \
-    -o /mnt2/recal_data.table \
+    -R /ephemeral/mnt/human_g1k_v37_decoy.fasta \
+    -I /ephemeral/mnt/NA12878.bam \
+    -knownSites /ephemeral/mnt/dbsnp_138.vcf \
+    -knownSites /ephemeral/mnt/Mills_and_1000G_gold_standard.indels.b37.vcf \
+    -knownSites /ephemeral/mnt/1000G_phase1.indels.b37.vcf \
+    -o /ephemeral/mnt/recal_data.table \
     -nct 32
+    > recalibrationTable.report 2>&1
 
 # recalibrate reads
-time java -Xmx200g \
+time java $RAM \
     -jar ~/gatk-protected/target/GenomeAnalysisTK.jar \
     -T PrintReads \
-    -R /mnt2/Homo_sapiens_assembly19.fasta \
-    -I /mnt2/NA12878.realigned.bam \
-    -BQSR /mnt2/recal_data.table \
-    -o /mnt2/NA12878.bqsr.bam \
+    -R /ephemeral/mnt/human_g1k_v37_decoy.fasta \
+    -I /ephemeral/mnt/NA12878.realigned.bam \
+    -BQSR /ephemeral/mnt/recal_data.table \
+    -o /ephemeral/mnt/NA12878.bqsr.bam \
     -nct 32
+    > Bqsr.report 2>&1
 
 # GATK VARIANT CALLING
 # two stages, check for snps, check for indels
 # When these stages are run for multiple bams, uncomment the desired number of -I slots
+# 1000Genomes Background BAMS can be run concurrently with UnifiedGenotyper to improve accuracy? (CEU,GBR are the recommended ones)
 
 #snps
-time java -Xmx200g
+time java $RAM
 	-jar /GenomeAnalysisTK-2.4-9-g532efad/GenomeAnalysisTK.jar \
-	-nt 32 \
-	-R /b37/Homo_sapiens_assembly19.fasta \
+	-nt $THREADS \
+	-R /b37/human_g1k_v37_decoy.fasta \
 	-T UnifiedGenotyper \
 	-I file1.recal.bam \
 	#-I file2.recal.bam \
@@ -155,12 +134,13 @@ time java -Xmx200g
 	-dcov 200 \
 	#-L /b37/target_intervals.bed \ intervals to operate over. See docstring
 	-glm SNP \
+	> SnpVars.report 2>&1
 
 #indels
-time java -Xmx200g
+time java $RAM
 	-jar /GenomeAnalysisTK-2.4-9-g532efad/GenomeAnalysisTK.jar \
-	-nt 32 \
-	-R /b37/Homo_sapiens_assembly19.fasta \
+	-nt $THREADS \
+	-R /b37/human_g1k_v37_decoy.fasta \
 	-T UnifiedGenotyper \
 	-I file1.recal.bam \
 	#-I file2.recal.bam \
@@ -173,12 +153,101 @@ time java -Xmx200g
 	-dcov 200 \
 	#-L /b37/target_intervals.bed \ #see docstring
 	-glm INDEL \
+	> indelVars.report 2>&1
 
-## NEXT APPROPRIATE STEP WOULD BE VARIANT QUALITY SCORE RECALIBRATION
+## GATK VARIANT QUALITY SCORE RECALIBRATION
+# Snp Recalibration
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T VariantRecalibrator
+  -R human_g1k_v37_decoy.fasta
+  -input Sample1.raw.vcf
+  -nt $THREADS
+  -resource: hapmap,known=false,training=true,truth=true,prior=15.0 hapmap_3.3.b37.sites.vcf
+  -resource: omni,known=false,training=true,truth=true,prior=12.0 1000G_omni2.5.b37.sites.vcf
+  -resource: dbsnp,known=false,training=true,truth=false,prior=6.0 dbsnp_135.b37.vcf
+  -use_annotation: QD
+  -use_annotation: HaplotypeScore
+  -use_annotation: MQRankSum
+  -use_annotation: ReadPosRankSum
+  -use_annotation: FS
+  -numBadVariants: 5000
+  -mode SNP                           
+  -recalFile Sample1_SNP.recal            
+  -tranchesFile Sample1_SNP.tranches      
+  -rscriptFile Sample1_SNP.plots.R       
+  > VariantRecalibrator_SNP.report 2>&1
 
+#Apply Snp Recalibration
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T ApplyRecalibration
+  -input Sample1_raw_SNP.vcf
+  -o Sample1_SNP_vqsr_SNP.vcf
+  -R human_g1k_v37_decoy.fasta
+  -nt $THREADS
+  -ts_filter_level: 99.0
+  -excludeFiltered : TRUE
+  -tranchesFile Sample1.tranches
+  -recalFile Sample1.recal
+  -mode SNP 
+  > ApplyRecalibration_SNP.report 2>&1
 
+#Indel Recalibration
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T VariantRecalibrator
+  -R human_g1k_v37_decoy.fasta
+  -input Sample1.raw.vcf
+  -nt $THREADS
+  -resource: mills,known=false,training=true,truth=true,prior=12.0 Mills_and_1000G_gold_standard.indels.b37.vcf 
+  -resource: 1000G,known=false,training=true,truth=true,prior=10.0 1000G_phase1.indels.b37.vcf
+  -use_annotation: MQRankSum
+  -use_annotation: ReadPosRankSum
+  -use_annotation: FS
+  -numBadVariants: 5000
+  -mode INDEL
+  -recalFile Sample1_INDEL.recal            
+  -tranchesFile Sample1_INDEL.tranches      
+  -rscriptFile Sample1_INDEL.plots.R       
+  > VariantRecalibrator_INDEL.report 2>&1
 
+#Apply Indel Recalibration
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T ApplyRecalibration
+  -input Sample1_raw_INDEL.vcf
+  -o Sample1_vqsr_INDEL.vcf
+  -R human_g1k_v37_decoy.fasta
+  -nt $THREADS
+  -ts_filter_level: 99.0
+  -excludeFiltered : TRUE
+  -tranchesFile Sample1.tranches
+  -recalFile Sample1.recal
+  -mode INDEL 
+  > ApplyRecalibration_INDEL.report 2>&1
 
+#SELECT VARIANTS
+#These steps remove the background files and output SNP and INDEL files, then combine them into a single VCF file
 
+#Select Snp
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T SelectVariants
+  -R human_g1k_v37_decoy.fasta
+  --variant SNP_variant 
+  -o output_selected_SNP.file 
+  > SelectVariants_SNP.report 2>&1
 
+#Select Indel
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T SelectVariants
+  -R human_g1k_v37_decoy.fasta
+  --variant INDEL_variant 
+  -o output_Selected_INDEL.file 
+  > SelectVariants_INDEL.report 2>&1
 
+#Combine Variants
+java $RAM -Djava.io.tmpdir=/tmp GenomeAnalysisTK.jar
+  -T CombineVariants
+  -R human_g1k_v37_decoy.fasta
+  --variant INDEL_variant 
+  --variant  output_Selected_INDEL.file
+  --variant output_Selected_SNP.file
+  -o Final_combined_variants.vcf 
+  > CombineVarients.report 2>&1
--- GATKsetup.sh
+++ GATKsetup.sh
@@ -1,6 +1,4 @@
-cd /mnt2
-# default 
-# change to ubuntu linux ami /ephemeral/mnt
+cd /ephemeral/mnt
 
 # get NA12878 from 1000g from BD2K's s3
 ~/s3cmd/s3cmd get \
@@ -9,17 +7,29 @@ cd /mnt2
     s3://bd2k-test-data/NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam
 mv NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam NA12878.bam
 
-# get reference genome                                                                                                                                                                                        
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta.fai
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta
+#get reference genome
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/human_g1k_v37_decoy.fasta.gz
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/human_g1k_v37_decoy.fasta.fai.gz
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/human_g1k_v37_decoy.dict.gz
+gunzip human_g1k_v37_decoy.fasta
+gunzip human_g1k_v37_decoy.fasta.fai 
+gunzip human_g1k_v37_decoy.dict.gz
 
-# get Known indels
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/1000G_phase1.indels.hg19.sites.fixed.vcf
+# VCF files for VariantRecalibrator_INDEL.RealignerTargetCreator knowns, and dbsnp for BaseRecalibraton
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/1000G_phase1.indels.b37.vcf.gz
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/dbsnp_137.b37.vcf.gz
+gunzip Mills_and_1000G_gold_standard.indels.b37.vcf.gz
+gunzip 1000G_phase1.indels.b37.vcf.gz
+gunzip dbsnp_137.b37.vcf.gz
 
-# get db snp known sites
-wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/dbsnp_138.hg19.fixed.vcf
-mv dbsnp_138.hg19.fixed.vcf dbsnp_138.vcf
+#Resource files for VariantRecalibrator_SNP
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/hapmap_3.3.b37.vcf.gz
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/1000G_omni2.5.b37.vcf.gz
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.5/b37/1000G_phase1.snps.high_confidence.b37.vcf.gz
+gunzip hapmap_3.3.b37.vcf.gz
+gunzip 1000G_omni2.5.b37.vcf.gz
+gunzip 1000G_phase1.snps.high_confidence.b37.vcf.gz
 
 
 cd ~
