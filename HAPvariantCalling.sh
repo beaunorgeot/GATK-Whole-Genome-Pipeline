263c6f86acf1b92e592a6d855db407f6584c3b8e
Beau Norgeot <beaunorgeot@gmail.com>
2015-05-13 10:57:49 -0700

    MQ flags removed

--- HAPvariantCalling.sh
+++ HAPvariantCalling.sh
@@ -17,8 +17,7 @@ dir=/data
 ref=hs37d5.fa
 
 # Create Variable for input file
-#INPUT1=$INPUT1.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906
-INPUT1=NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam
+INPUT1=NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bam
 #choose how to log time
 Time=/usr/bin/time
 
@@ -34,7 +33,7 @@ $Time java $RAM \
 --genotyping_mode DISCOVERY \
 --output_mode EMIT_VARIANTS_ONLY \
 -I ${dir}/$INPUT1.bqsr.bam \
--o ${dir}/$INPUT1.unified.raw.SNP.gatk.vcf \
+-o ${dir}/$INPUT1.unified.raw.BOTH.gatk.vcf \
 -stand_emit_conf 10.0 \
 -stand_call_conf 30.0 \
 > HapVars.report 2>&1
@@ -51,7 +50,7 @@ $Time java $RAM \
   -jar ~/GenomeAnalysisTK.jar \
   -T VariantRecalibrator \
   -R ${dir}/${ref} \
-  -input ${dir}/$INPUT1.unified.raw.SNP.gatk.vcf \
+  -input ${dir}/$INPUT1.unified.raw.BOTH.gatk.vcf \
   -nt $THREADS \
   -resource:hapmap,known=false,training=true,truth=true,prior=15.0 ${dir}/hapmap_3.3.b37.vcf \
   -resource:omni,known=false,training=true,truth=false,prior=12.0 ${dir}/1000G_omni2.5.b37.vcf \
@@ -60,60 +59,58 @@ $Time java $RAM \
   -an QD \
   -an DP \
   -an FS \
-  -an MQRankSum \
   -an ReadPosRankSum \
   -mode SNP \
   -minNumBad 1000 \
-  -recalFile ${dir}/$INPUT.SNP.recal \
-  -tranchesFile ${dir}/$INPUT.SNP.tranches \
-  -rscriptFile ${dir}/$INPUT.SNP.plots.R \
-  > VariantRecalibrator_SNP.report 2>&1
+  -recalFile ${dir}/$INPUT.HAPSNP.recal \
+  -tranchesFile ${dir}/$INPUT.HAPSNP.tranches \
+  -rscriptFile ${dir}/$INPUT.HAPSNP.plots.R \
+  > HAPVariantRecalibrator_HAPSNP.report 2>&1
   #-percentBad 0.01 \ depreciated
 
 #Apply Snp Recalibration
 $Time java $RAM \
   -jar ~/GenomeAnalysisTK.jar \
   -T ApplyRecalibration \
-  -input ${dir}/$INPUT1.unified.raw.SNP.gatk.vcf \
-  -o ${dir}/$INPUT1.SNP.vqsr.SNP.vcf \
+  -input ${dir}/$INPUT1.unified.raw.BOTH.gatk.vcf \
+  -o ${dir}/$INPUT1.HAPSNP.vqsr.SNP.vcf \
   -R ${dir}/${ref} \
   -nt $THREADS \
   -ts_filter_level 99.0 \
-  -tranchesFile ${dir}/$INPUT.SNP.tranches \
-  -recalFile ${dir}/$INPUT.SNP.recal \
+  -tranchesFile ${dir}/$INPUT.HAPSNP.tranches \
+  -recalFile ${dir}/$INPUT.HAPSNP.recal \
   -mode SNP \
-  > ApplyRecalibration_SNP.report 2>&1
+  > HAPApplyRecalibration_HAPSNP.report 2>&1
 
 #Indel Recalibration
 $Time java $RAM -Djava.io.tmpdir=/tmp \
   -jar ~/GenomeAnalysisTK.jar \
   -T VariantRecalibrator \
   -R ${dir}/${ref} \
-  -input ${dir}/$INPUT1.unified.raw.SNP.gatk.vcf \
+  -input ${dir}/$INPUT1.unified.raw.BOTH.gatk.vcf \
   -nt $THREADS \
   -resource:mills,known=true,training=true,truth=true,prior=12.0 ${dir}/Mills_and_1000G_gold_standard.indels.b37.vcf \
   -an DP \
   -an FS \
-  -an MQRankSum \
   -an ReadPosRankSum \
   -mode INDEL \
   -minNumBad 1000 \
-  -recalFile ${dir}/$INPUT.INDEL.recal \
-  -tranchesFile ${dir}/$INPUT.INDEL.tranches \
-  -rscriptFile ${dir}/$INPUT.INDEL.plots.R \
-  > VariantRecalibrator_INDEL.report 2>&1
+  -recalFile ${dir}/$INPUT.HAPINDEL.recal \
+  -tranchesFile ${dir}/$INPUT.HAPINDEL.tranches \
+  -rscriptFile ${dir}/$INPUT.HAPINDEL.plots.R \
+  > HAPVariantRecalibrator_HAPINDEL.report 2>&1
   #-percentBad 0.01 \depreciated
 
 #Apply Indel Recalibration
 $Time java $RAM \
   -jar ~/GenomeAnalysisTK.jar \
   -T ApplyRecalibration \
-  -input ${dir}/$INPUT1.unified.raw.INDEL.gatk.vcf \
+  -input ${dir}/$INPUT1.unified.raw.BOTH.gatk.vcf \
   -o ${dir}/$INPUT.vqsr_INDEL.vcf \
   -R ${dir}/${ref} \
   -nt $THREADS \
   -ts_filter_level 99.0 \
-  -tranchesFile ${dir}/$INPUT.INDEL.tranches \
-  -recalFile ${dir}/$INPUT.INDEL.recal \
+  -tranchesFile ${dir}/$INPUT.HAPINDEL.tranches \
+  -recalFile ${dir}/$INPUT.HAPINDEL.recal \
   -mode INDEL \
-  > ApplyRecalibration_INDEL.report 2>&1
+  > HAPApplyRecalibration_HAPINDEL.report 2>&1
