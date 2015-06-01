b6739ae591e9a4ad7b0f18d19d15844c8b559d04
Beau Norgeot <beaunorgeot@gmail.com>
2015-06-01 13:09:51 -0700

    all hdfs files now on hdfs root

--- speedyADAM.sh
+++ speedyADAM.sh
@@ -22,7 +22,6 @@ curl -O https://s3-us-west-2.amazonaws.com/bd2k-artifacts/adam/adam-core_2.10-0.
 Time=/usr/bin/time
 ADAM_HOME=~/adam-core_2.10-0.16.1-SNAPSHOT.9f0f41a.jar
 hdfs_root=hdfs://spark-master:8020
-USER=ubuntu
 Input1=FEEDME
 
 # pull $Input1 from s3 using s3-downloader
@@ -36,67 +35,67 @@ curl -O https://s3-us-west-2.amazonaws.com/bd2k-test-data/dbsnp132_20101103.vcf.
 gunzip dbsnp132_20101103.vcf.gz
 mv dbsnp132_20101103.vcf dbsnp_132.vcf
 #put file into hdfs
-/opt/sparkbox/hadoop/bin/hadoop fs -put ${hdfs_root}/user/${USER}/dbsnp_132.vcf
+/opt/sparkbox/hadoop/bin/hadoop fs -put ${hdfs_root}/dbsnp_132.vcf
 
 # convert known snps file to adam variants file
 $Time ${ADAM_HOME}/bin/adam-submit vcf2adam \
-    ${hdfs_root}/user/${USER}/dbsnp_132.vcf \
-    ${hdfs_root}/user/${USER}/dbsnp_132.var.adam \
+    ${hdfs_root}/dbsnp_132.vcf \
+    ${hdfs_root}/dbsnp_132.var.adam \
     -onlyvariants \
     > convert_known_snps_2adam.report 2>&1
 
 #remove known snps vcf
 /opt/sparkbox/hadoop/bin/hadoop fs -rmr \
-    ${hdfs_root}/user/${USER}/dbsnp_132.vcf
+    ${hdfs_root}/dbsnp_132.vcf
 
 # START PIPELINE
 
 # convert to adam, and remove bam
 $Time ${ADAM_HOME}/bin/adam-submit transform \
-    ${hdfs_root}/user/${USER}/$Input1.bam \
-    ${hdfs_root}/user/${USER}/$Input1.adam \
+    ${hdfs_root}/$Input1.bam \
+    ${hdfs_root}/$Input1.adam \
     > convert_bam2adam.report 2>&1
 
 /opt/sparkbox/hadoop/bin/hadoop fs -rmr \
-    ${hdfs_root}/user/${USER}/$Input1.bam
+    ${hdfs_root}/$Input1.bam
 
 # mark duplicate reads
 $Time ${ADAM_HOME}/bin/adam-submit transform \
-    ${hdfs_root}/user/${USER}/$Input1.adam \
-    ${hdfs_root}/user/${USER}/$Input1.mkdup.adam \
+    ${hdfs_root}/$Input1.adam \
+    ${hdfs_root}/$Input1.mkdup.adam \
     -mark_duplicate_reads \
     > markDups.report 2>&1
 
 #remove .adam input file
 /opt/sparkbox/hadoop/bin/hadoop fs -rmr \
-    ${hdfs_root}/user/${USER}/$Input1.adam
+    ${hdfs_root}/$Input1.adam
 
 # realign indels
 $Time ${ADAM_HOME}/bin/adam-submit transform \
-    ${hdfs_root}/user/${USER}/$Input1.mkdup.adam \
-    ${hdfs_root}/user/${USER}/$Input1.ri.adam \
+    ${hdfs_root}/$Input1.mkdup.adam \
+    ${hdfs_root}/$Input1.ri.adam \
     -realign_indels \
     > realignIndels.report 2>&1
 
 #remove mkdup.adam input
 /opt/sparkbox/hadoop/bin/hadoop fs -rmr \
-    ${hdfs_root}/user/${USER}/$Input1.mkdup.adam
+    ${hdfs_root}/$Input1.mkdup.adam
 
 # recalibrate quality scores
 $Time ${ADAM_HOME}/bin/adam-submit transform \
-    ${hdfs_root}/user/${USER}/$Input1.ri.adam \
-    ${hdfs_root}/user/${USER}/$Input1.bqsr.adam \
+    ${hdfs_root}/$Input1.ri.adam \
+    ${hdfs_root}/$Input1.bqsr.adam \
     -recalibrate_base_qualities \
-    -known_snps ${hdfs_root}/user/${USER}/dbsnp_132.var.adam \
+    -known_snps ${hdfs_root}/dbsnp_132.var.adam \
     > BQSR.report 2>&1
 
 /opt/sparkbox/hadoop/bin/hadoop fs -rmr \
-    ${hdfs_root}/user/${USER}/dbsnp_132.var.adam
+    ${hdfs_root}/dbsnp_132.var.adam
 
 # Convert .adam file back to bam for external Genotyper
 $Time ${ADAM_HOME}/bin/adam-submit transform \
-    ${hdfs_root}/user/${USER}/$Input1.bqsr.adam \
-    ${hdfs_root}/user/${USER}/$Input1.bqsr.bam \
+    ${hdfs_root}/$Input1.bqsr.adam \
+    ${hdfs_root}/$Input1.bqsr.bam \
     > convert_adam2bam.report 2>&1
 
 #HERE BEGINS VARIANT CALLING STEPS USING .bqsr.bam as the input
