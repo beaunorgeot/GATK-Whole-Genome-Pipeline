59ea096d8bce261b53574be313cdddc35332439d
Beau Norgeot <beaunorgeot@gmail.com>
2015-08-06 11:22:54 -0700

    file movements

--- reOderBam.sh
+++ reOderBam.sh
@@ -1,5 +1,11 @@
+cd /data/NAparquet/NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906.bqsr.bam
+#cat the parts into a single bam mv the file to the main dir
+samtools cat -o combinedNA12878.bam part-r-*
+mv combinedNA12878.bam /data/
 
-#Perform this in /data (or the dir that has the bad bam and the reference)
+
+#Perform reorder in /data (or the dir that has the bad bam and the new reference)
+cd /data
 
 java -jar ~/picard.jar ReorderSam \
     I=combinedNA12878.bam \
