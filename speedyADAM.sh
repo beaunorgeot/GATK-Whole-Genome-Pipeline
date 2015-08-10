12757481e187f3f0ef7dad228f376929f22fb38c
Beau Norgeot <beaunorgeot@gmail.com>
2015-08-10 11:14:42 -0700

    added sort stage, s3 up/down load changes

--- speedyADAM.sh
+++ speedyADAM.sh
@@ -7,12 +7,18 @@ set -e
 set -x
 set -o pipefail
 
-executor_memory=55G
+executor_memory=110G
 export ADAM_DRIVER_MEMORY=${executor_memory}
 export ADAM_EXECUTOR_MEMORY=${executor_memory}
 export SPARK_HOME="/opt/sparkbox/spark"
 export ADAM_OPTS="--conf spark.eventLog.enabled=true --conf spark.worker.timeout=500 --conf spark.driver.maxResultSize=10g"
 #export ADAM_OPTS="--conf spark.shuffle.service.enable=true"
+
+Time=/usr/bin/time
+export ADAM_HOME=~/adam-distribution_2.10-0.17.0
+export hdfs_root=hdfs://spark-master:8020
+Input1=FEEDME
+#NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906
 
 #Get spark -s3 downloader
 curl -O https://s3-us-west-2.amazonaws.com/bd2k-artifacts/adam/spark-s3-downloader-0.3-SNAPSHOT.jar
@@ -21,18 +27,12 @@ curl -O https://s3-us-west-2.amazonaws.com/bd2k-artifacts/adam/spark-s3-download
 curl -LOv http://search.maven.org/remotecontent?filepath=org/bdgenomics/adam/adam-distribution_2.10/0.17.0/adam-distribution_2.10-0.17.0-bin.tar.gz
 tar -xvzf adam-distribution_2.10-0.17.0-bin.tar.gz
 
-
-Time=/usr/bin/time
-export ADAM_HOME=~/adam-distribution_2.10-0.17.0
-export hdfs_root=hdfs://spark-master:8020
-Input1=FEEDME
-#NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130906
-
 # pull $Input1 from s3 using s3-downloader
 /opt/sparkbox/spark/bin/spark-submit --executor-memory ${executor_memory} \
-    ~/spark-s3-downloader-0.2-SNAPSHOT.jar \
+    ~/spark-s3-downloader-0.3-SNAPSHOT.jar \
     s3://bd2k-test-data/${Input1}.bam \
-    ${hdfs_root}/${Input1}.bam
+    ${hdfs_root}/${Input1}.bam \
+    --concat #files must be explicitly cat'd
 
 # download dbsnp file for bqsr step
 curl -O https://s3-us-west-2.amazonaws.com/bd2k-test-data/dbsnp132_20101103.vcf.gz
@@ -70,16 +70,27 @@ $Time ${ADAM_HOME}/bin/adam-submit transform \
 /opt/sparkbox/hadoop/bin/hadoop fs -rm -R \
     ${hdfs_root}/$Input1.bam
 
-# mark duplicate reads
+# sort the file
 $Time ${ADAM_HOME}/bin/adam-submit transform \
-    ${hdfs_root}/$Input1.adam \
-    ${hdfs_root}/$Input1.mkdup.adam \
-    -mark_duplicate_reads \
-    > markDups.report 2>&1
+    ${hdfs_root}/user/${USER}/NA12878.adam \
+    ${hdfs_root}/user/${USER}/NA12878.sort.adam \
+    -sort_reads \
+    > sort.report 2>&1
 
 #remove .adam input file
 /opt/sparkbox/hadoop/bin/hadoop fs -rm -R \
     ${hdfs_root}/$Input1.adam
+
+# mark duplicate reads
+$Time ${ADAM_HOME}/bin/adam-submit transform \
+    ${hdfs_root}/$Input1.sort.adam \
+    ${hdfs_root}/$Input1.mkdup.adam \
+    -mark_duplicate_reads \
+    > markDups.report 2>&1
+
+#remove sort.adam input file
+/opt/sparkbox/hadoop/bin/hadoop fs -rm -R \
+    ${hdfs_root}/$Input1.sort.adam
 
 # realign indels
 $Time ${ADAM_HOME}/bin/adam-submit transform \
@@ -110,3 +121,11 @@ $Time ${ADAM_HOME}/bin/adam-submit transform \
     > convert_adam2bam.report 2>&1
 
 #HERE BEGINS VARIANT CALLING STEPS USING .bqsr.bam as the input
+
+# upload to s3 using spark-loader
+$Time /opt/sparkbox/spark/bin/spark-submit --executor-memory ${executor_memory} \
+    ~/spark-s3-downloader-0.3-SNAPSHOT.jar \
+    ${hdfs_root}/${Input1}.bqsr.bam \
+    s3://bd2k-test-data/${Input1}.bqsr.attempt2.bam \
+    > upload.report 2>&1
+
